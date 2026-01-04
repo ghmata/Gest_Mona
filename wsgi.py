df132c27d8e218298b338ad93ffb2e0d9db1cea3
GABRIEL <gabrielhm00@gmail.com>
2026-01-04 07:59:37 -0300

    Fix WSGI path for PythonAnywhere: /home/Hip00/MONA_Controle_financeiro

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,13 @@ Arquivo WSGI para PythonAnywhere.
 
 Este arquivo é o ponto de entrada para o servidor WSGI do PythonAnywhere.
 Configure no PythonAnywhere:
-  Source code: /home/SEU_USUARIO/MONA_v1
-  Working directory: /home/SEU_USUARIO/MONA_v1
-  WSGI configuration file: /var/www/SEU_USUARIO_pythonanywhere_com_wsgi.py
+  Source code: /home/Hip00/MONA_Controle_financeiro
+  Working directory: /home/Hip00/MONA_Controle_financeiro
+  WSGI configuration file: /var/www/Hip00_pythonanywhere_com_wsgi.py
 
 No arquivo WSGI do PythonAnywhere, adicione:
   import sys
-  sys.path.insert(0, '/home/SEU_USUARIO/MONA_v1')
+  sys.path.insert(0, '/home/Hip00/MONA_Controle_financeiro')
   from wsgi import application
 """
 
@@ -17,12 +17,17 @@ import sys
 import os
 
 # Adiciona o diretório do projeto ao path
-project_home = os.path.dirname(os.path.abspath(__file__))
+project_home = '/home/Hip00/MONA_Controle_financeiro'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
+# Define o diretório atual para garantir imports relativos
+if os.path.isdir(project_home):
+    os.chdir(project_home)
+
 # Importa a aplicação Flask
 from app import app as application
+
 
 # Inicializa o banco de dados
 with application.app_context():
